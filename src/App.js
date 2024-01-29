a2f5374e702d63dce44766eda1a0a21a949fd147
Shanmugalakshmi11 <shanmugalakshmi.chandrasekaran@docc.techstarter.de>
2024-01-29 14:43:04 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit shalaa <code>src/App.js</code> and save to reload.
+          Edit shalaa react<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
